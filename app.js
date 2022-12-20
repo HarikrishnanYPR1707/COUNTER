5baad855576f164e00fe710ee866661fc634f91b
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2022-12-20 15:58:14 +0530

    Modified app.js and completed with the project --> COUNTER <--

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ btns.forEach((btn) => {
 			count = 0;
 		}
 
+		if(count > 0) {
+			value.style.color = 'green';
+		} else if (count < 0) {
+			value.style.color = 'red';
+		} else {
+			value.style.color = 'blue';
+		}
+		
 		value.textContent = count;
 	})
 })
